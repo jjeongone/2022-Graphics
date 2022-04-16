2205b19ae10e934299831d766ef0dc6ea3d8aa17
cjw000221 <jjeongone@gmail.com>
2022-04-17 00:25:59 +0900

    implement sphere bullet

--- ASSN3/bullet.cpp
+++ ASSN3/bullet.cpp
@@ -1,94 +1,58 @@
 #include "bullet.h"
 
 Bullet::Bullet() {
-	x = 0.0;
-	y = 0.0;
+	x = 0.0f;
+	y = 0.0f;
+	z = 0.0f;
 }
 
-Bullet::Bullet(float init_x, float init_y, float init_speed, float init_angle) {
+Bullet::Bullet(float init_x, float init_y, float init_z, float init_speed, float init_angle) {
 	x = init_x;
 	y = init_y;
+	z = init_z;
 	x_speed = init_speed * std::cos(init_angle);
 	y_speed = abs(init_speed * std::sin(init_angle));
+	z_speed = init_speed * std::cos(init_angle);
 }
 
-std::pair<float, float> Bullet::position() {
-	return std::pair<float, float>(x, y);
+std::tuple<float, float, float> Bullet::position() {
+	return std::tuple<float, float, float>(x, y, z);
 }
 
 void Bullet::move() {
 	x += x_speed;
 	y += y_speed;
+	z += z_speed;
 }
 
-void Bullet::draw() {
-	glColor3f(1.0, 1.0, 1.0);
-	glBegin(GL_POLYGON);
-	glVertex2f(x, y);
-	glVertex2f(x, y + radius);
-	glVertex2f(x + radius, y + radius);
-	glVertex2f(x + radius, y);
-	glEnd();
+void Bullet::draw_bullet(bool fill) {
+	cout << fill << endl;
+	glPushMatrix();
+	glTranslatef(x, y, z);
+	if (fill)
+		glColor3f(0.8f, 0.8f, 0.8f);
+	else
+		glColor3f(1.0f, 1.0f, 1.0f);
+	for (int i = 0; i < bullet_vertices.size(); i++) {
+		glBegin(fill ? GL_TRIANGLES : GL_LINE_LOOP);
+		glVertex3f(bullet_vertices[i].x, bullet_vertices[i].y, bullet_vertices[i].z);
+		i++;
+		glVertex3f(bullet_vertices[i].x, bullet_vertices[i].y, bullet_vertices[i].z);
+		i++;
+		glVertex3f(bullet_vertices[i].x, bullet_vertices[i].y, bullet_vertices[i].z);
+		glEnd();
+	}
+	glPopMatrix();
 }
 
 void Bullet::changeSpeed() {
 	y_speed -= GRAVITY;
 }
 
-std::pair<float, float> Bullet::getSpeed() {
-	return std::make_pair(x_speed, y_speed);
+std::tuple<float, float, float> Bullet::getSpeed() {
+	return std::make_tuple(x_speed, y_speed, z_speed);
 }
 
 bool Bullet::isExplode(Tank* player, Tank* enemy) {
-	std::pair<std::pair<float, float>, std::pair<float, float>> player_body_hitbox;
-	player_body_hitbox.first = make_pair(player->coordinate.first, player->coordinate.second - player->size / 12.);
-	player_body_hitbox.second = make_pair(player->coordinate.first + player->size, player->coordinate.second + 0.26 * player->size);
-
-	std::pair<std::pair<float, float>, std::pair<float, float>> player_turret_hitbox;
-	player_turret_hitbox.first = make_pair(player->coordinate.first + player->size * 0.1, player->coordinate.second + player->size * 0.26 * 0.95);
-	player_turret_hitbox.second = make_pair(player->coordinate.first + player->size * 0.5, player->coordinate.second + player->size * (0.26 * 0.95 + 0.2));
-
-	std::pair<std::pair<float, float>, std::pair<float, float>> enemy_body_hitbox;
-	enemy_body_hitbox.first = make_pair(enemy->coordinate.first, enemy->coordinate.second - enemy->size / 12.);
-	enemy_body_hitbox.second = make_pair(enemy->coordinate.first - enemy->size, enemy->coordinate.second + 0.26 * enemy->size);
-
-	std::pair<std::pair<float, float>, std::pair<float, float>> enemy_turret_hitbox;
-	enemy_turret_hitbox.first = make_pair(enemy->coordinate.first - enemy->size * 0.1, enemy->coordinate.second + enemy->size * 0.26 * 0.95);
-	enemy_turret_hitbox.second = make_pair(enemy->coordinate.first - enemy->size * 0.5, enemy->coordinate.second + enemy->size * (0.26 * 0.95 + 0.2));
-
-	// player hitbox
-	if (this->x > player_body_hitbox.first.first && this->x < player_body_hitbox.second.first && this->y > player_body_hitbox.first.second && this->y < player_body_hitbox.second.second)
-	{
-		int current = player->getHealth();
-		std::cout << current;
-		player->setHealth(player->getHealth() - 1);
-		return true;
-	}
-	else if (this->x > player_turret_hitbox.first.first && this->x < player_turret_hitbox.second.first && this->y > player_turret_hitbox.first.second && this->y < player_turret_hitbox.second.second)
-	{
-		std::cout << "2";
-		player->setHealth(player->getHealth() - 1);
-		return true;
-	}
-	// enemy hitbox
-	else if (this->x < enemy_body_hitbox.first.first && this->x > enemy_body_hitbox.second.first && this->y > enemy_body_hitbox.first.second && this->y < enemy_body_hitbox.second.second)
-	{
-		std::cout << "3";
-		enemy->setHealth(enemy->getHealth() - 1);
-		return true;
-	}
-	else if (this->x < enemy_turret_hitbox.first.first && this->x > enemy_turret_hitbox.second.first && this->y > enemy_turret_hitbox.first.second && this->y < enemy_turret_hitbox.second.second)
-	{
-		std::cout << "4";
-		enemy->setHealth(enemy->getHealth() - 1);
-		return true;
-	}
-	else if (this->y < player->getBottom())
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+	return false;
 }
--- ASSN3/bullet.h
+++ ASSN3/bullet.h
@@ -9,25 +9,29 @@
 #include <cmath>
 #include <vector>
 #include <utility>
+#include <tuple>
 #include "tank.h"
 
 #define GRAVITY 0.00002
 
+
 class Bullet {
 private:
-	float x;
-	float y;
-	float radius = 0.05;
+	float x, y, z;
+	float radius = 1.8f;
 	float x_speed = 0.006 * std::cos(30. * 3.142 / 180);
 	float y_speed = 0.006 * std::sin(30. * 3.142 / 180);
+	float z_speed = 0.006 * std::cos(30. * 3.142 / 180);
+	Loader* bullet = new Loader("./model/bullet.obj");
+	std::vector < glm::vec3 > bullet_vertices = bullet->get_vertex();
 public:
 	Bullet();
-	Bullet(float init_x, float init_y, float init_speed, float init_angle);
-	std::pair<float,float> position();
-	void draw();
+	Bullet(float init_x, float init_y, float init_z, float init_speed, float init_angle);
+	std::tuple<float,float, float> position();
+	void draw_bullet(bool fill);
 	void move();
 	void changeSpeed();
-	std::pair<float,float> getSpeed();
+	std::tuple<float,float, float> getSpeed();
 	bool isExplode(Tank* player, Tank* enemy);
 };
 
